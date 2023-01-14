7d5cb85ee915601684500d8fc8451c99857263d4
LMH <90649822+LoneHilke@users.noreply.github.com>
2023-01-14 19:57:45 +0100

    igennem
    færdig siden
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- movielist/movies/views.py
+++ movielist/movies/views.py
@@ -4,3 +4,8 @@ from django.views.generic import TemplateView
 
 class MovieView(TemplateView):
     template_name = 'index.html'
+
+
+
+
+# fra; https://www.bugbytes.io/posts/django-unicorn-an-introduction/
